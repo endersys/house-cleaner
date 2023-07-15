d7e1410122c670ee3e7eed5347705b7b98ce9ac3
Enderson <enderson.sys@gmail.com>
2023-07-15 14:38:42 -0300

    feat: custom theme

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+import colors from 'tailwindcss/colors' 
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography' 
+
+export default {
+  content: [
+	'./resources/**/*.blade.php',
+	'./vendor/filament/**/*.blade.php', 
+  ],
+  theme: {
+    extend: {
+		colors: { 
+			danger: colors.rose,
+			primary: colors.blue,
+			success: colors.green,
+			warning: colors.yellow,
+		}, 
+	},
+  },
+  plugins: [
+	forms, 
+	typography, 
+  ],
+}
